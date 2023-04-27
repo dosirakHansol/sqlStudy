2c81837db8a85dba2527f3e7f99b6bc2331ec913
DESKTOP-69UC1S4\박한솔 <crying_beg@naver.com>
2023-04-27 22:50:08 +0900

    오늘의 문제풀이 SELECT 문 주요내용 : 서브쿼리 이용한 ROWNUM 활용

--- PROGRAMMERS.sql
+++ PROGRAMMERS.sql
@@ -77,4 +77,58 @@ ORDER BY
     , PRODUCT_ID ASC
     , USER_ID ASC
     
-    
+/*
+    동물 보호소에 들어온 동물 중 아픈 동물1의 아이디와 이름을 조회하는 SQL 문을 작성
+    - 결과는 아이디 순으로 조회
+*/
+SELECT
+    ANIMAL_ID
+    , NAME
+FROM
+    ANIMAL_INS
+WHERE 1=1
+    AND INTAKE_CONDITION = 'Sick'
+ORDER BY
+    ANIMAL_ID ASC
+/*
+    동물 보호소에 들어온 동물 중 젊은 동물1의 아이디와 이름을 조회하는 SQL 문을 작성
+    - 결과는 아이디 순으로 조회
+*/
+SELECT
+    ANIMAL_ID
+    , NAME
+FROM
+    ANIMAL_INS
+WHERE 1=1
+    AND INTAKE_CONDITION != 'Aged'
+ORDER BY
+    ANIMAL_ID ASC
+/*
+    동물 보호소에 들어온 모든 동물의 아이디와 이름, 보호 시작일을 이름 순으로 조회하는 SQL문을 작성
+    - 이름이 같은 동물 중에서는 보호를 나중에 시작한 동물을 먼저 보여줘야 합니다.
+*/
+SELECT
+    ANIMAL_ID
+    , NAME
+    , DATETIME
+FROM
+    ANIMAL_INS
+ORDER BY
+    NAME ASC
+    , DATETIME DESC
+/*
+    동물 보호소에 가장 먼저 들어온 동물의 이름을 조회하는 SQL 문을 작성해주세요.
+*/
+SELECT
+    NAME
+FROM
+(
+    SELECT
+        NAME
+    FROM
+        ANIMAL_INS
+    ORDER BY 
+        DATETIME ASC
+) 
+WHERE 1=1
+    AND ROWNUM = 1
