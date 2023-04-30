e79ede59061bd8a45270ca23c714af1d03867888
DESKTOP-69UC1S4\박한솔 <crying_beg@naver.com>
2023-04-30 22:42:52 +0900

    오늘의 공부!

--- PROGRAMMERS.sql
+++ PROGRAMMERS.sql
@@ -235,3 +235,38 @@ ORDER BY
     SCORE DESC
     , FAVORITES DESC
 ;
+
+/*
+    PATIENT 테이블에서 12세 이하인 여자환자의 
+    환자이름, 환자번호, 성별코드, 나이, 전화번호를 조회하는 SQL문을 작성해주세요.
+    이때 전화번호가 없는 경우, 'NONE'으로 출력시켜 주시고 
+    결과는 나이를 기준으로 내림차순 정렬하고, 
+    나이 같다면 환자이름을 기준으로 오름차순 정렬해주세요.
+*/
+SELECT 
+    PT_NAME
+    , PT_NO
+    , GEND_CD
+    , AGE
+    , NVL(TLNO,'NONE')
+FROM PATIENT
+WHERE 1=1
+    AND AGE <= 12
+    AND GEND_CD = 'W'
+ORDER BY 
+    AGE DESC
+    , PT_NAME ASC
+    
+/*
+    상반기 아이스크림 총주문량이 3,000보다 높으면서 
+    아이스크림의 주 성분이 과일인 아이스크림의 맛을 
+    총주문량이 큰 순서대로 조회하는 SQL 문을 작성해주세요.
+*/
+SELECT
+    FIRST_HALF.FLAVOR
+FROM FIRST_HALF
+LEFT JOIN ICECREAM_INFO
+    ON ICECREAM_INFO.FLAVOR = FIRST_HALF.FLAVOR
+WHERE 1=1
+    AND FIRST_HALF.TOTAL_ORDER >= 3000
+    AND ICECREAM_INFO.INGREDIENT_TYPE = 'fruit_based'
