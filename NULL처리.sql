4ca408a83047c7a26ec55c6fc2ecf24c9ca31795
DESKTOP-69UC1S4\박한솔 <crying_beg@naver.com>
2023-05-09 21:05:48 +0900

    깃 오류로인하여... sql 공부

---
+++ NULL처리.sql
@@ -0,0 +1,12 @@
+/*
+    FOOD_WAREHOUSE 테이블에서 경기도에 위치한 창고의 ID, 이름, 주소, 냉동시설 여부를 조회하는 SQL문을 작성해주세요.
+    이때 냉동시설 여부가 NULL인 경우, 'N'으로 출력시켜 주시고 결과는 창고 ID를 기준으로 오름차순 정렬해주세요.
+*/
+SELECT 
+    WAREHOUSE_ID
+    , WAREHOUSE_NAME
+    , ADDRESS
+    , nvl(FREEZER_YN, 'N') as FREEZER_YN
+from FOOD_WAREHOUSE
+where SUBSTR(ADDRESS, 0, 3) = '경기도'
+order by WAREHOUSE_ID;
