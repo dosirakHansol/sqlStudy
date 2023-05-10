b035edd72d4665bb0d09859b905b8e17107b7753
DESKTOP-69UC1S4\박한솔 <crying_beg@naver.com>
2023-05-10 21:09:31 +0900

    널 처리할게 끝~

--- NULL처리.sql
+++ NULL처리.sql
@@ -10,3 +10,43 @@ SELECT
 from FOOD_WAREHOUSE
 where SUBSTR(ADDRESS, 0, 3) = '경기도'
 order by WAREHOUSE_ID;
+
+/*
+    동물 보호소에 들어온 동물 중, 이름이 없는 채로 들어온 동물의 ID를 조회하는 SQL 문을 작성해주세요. 
+    단, ID는 오름차순 정렬되어야 합니다.
+*/
+SELECT
+    ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME IS NULL
+ORDER BY ANIMAL_ID ASC
+
+/*
+    동물 보호소에 들어온 동물 중, 이름이 있는 동물의 ID를 조회하는 SQL 문을 작성해주세요. 
+    단, ID는 오름차순 정렬되어야 합니다.
+*/
+SELECT
+    ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL
+ORDER BY ANIMAL_ID ASC
+
+/*
+    동물의 생물 종, 이름, 성별 및 중성화 여부를 아이디 순으로 조회하는 SQL문을 작성해주세요.
+    이름이 없는 동물의 이름은 "No name"으로 표시해 주세요.
+*/
+SELECT
+    ANIMAL_TYPE
+    , NVL(NAME,'No name') AS NAME
+    , SEX_UPON_INTAKE
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID ASC
+
+/*
+    USER_INFO 테이블에서 나이 정보가 없는 회원이 몇 명인지 출력하는 SQL문을 작성해주세요. 
+    이때 컬럼명은 USERS로 지정해주세요.
+*/
+SELECT
+    COUNT(*) AS USERS
+FROM USER_INFO
+WHERE AGE IS NULL
