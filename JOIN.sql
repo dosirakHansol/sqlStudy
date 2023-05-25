59a20d07f59c4214c2ff80df9c2dfbab0258715e
DESKTOP-69UC1S4\박한솔 <crying_beg@naver.com>
2023-05-25 20:53:35 +0900

    오늘은 이것만...

--- JOIN.sql
+++ JOIN.sql
@@ -151,3 +151,21 @@ WHERE 1=1
     AND I.SEX_UPON_INTAKE LIKE 'Intact%'
     AND O.SEX_UPON_OUTCOME NOT LIKE 'Intact%'
 ORDER BY I.ANIMAL_ID ASC
+
+/*
+    PRODUCT 테이블과 OFFLINE_SALE 테이블에서 상품코드 별 매출액(판매가 * 판매량) 합계를 출력하는 SQL문을 작성해주세요.
+    결과는 매출액을 기준으로 내림차순 정렬해주시고 매출액이 같다면 상품코드를 기준으로 오름차순 정렬해주세요.
+*/
+SELECT 
+    P.PRODUCT_CODE
+    , S.AMOUNT * P.PRICE SALES
+FROM PRODUCT P
+INNER JOIN 
+    (
+        SELECT
+            PRODUCT_ID
+            , SUM(SALES_AMOUNT) AMOUNT
+        FROM OFFLINE_SALE
+        GROUP BY PRODUCT_ID
+    ) S ON S.PRODUCT_ID = P.PRODUCT_ID
+ORDER BY SALES DESC, PRODUCT_CODE ASC
