62026d0aa388d6d57b0c551ae5d5391f55748230
DESKTOP-69UC1S4\박한솔 <crying_beg@naver.com>
2023-05-31 23:03:30 +0900

    내일은 이사!

--- JOIN.sql
+++ JOIN.sql
@@ -240,3 +240,20 @@ NATURAL JOIN (
 ) A
 WHERE A.RANK = 1
 ORDER BY REVIEW_DATE, REVIEW_TEXT ASC
+
+/*
+    CAR_RENTAL_COMPANY_DISCOUNT_PLAN 테이블에서 자동차 종류가 '세단' 또는 'SUV' 인 자동차 중 2022년 11월 1일부터 2022년 11월 30일까지 대여 가능하고 30일간의 대여 금액이 50만원 이상 200만원 미만인 자동차에 대해서 자동차 ID, 자동차 종류, 대여 금액(컬럼명: FEE) 리스트를 출력하는 SQL문을 작성해주세요. 결과는 대여 금액을 기준으로 내림차순 정렬하고, 대여 금액이 같은 경우 자동차 종류를 기준으로 오름차순 정렬, 자동차 종류까지 같은 경우 자동차 ID를 기준으로 내림차순 정렬해주세요.
+*/
+SELECT *
+FROM (SELECT CAR_ID, CAR_TYPE, (DAILY_FEE*30*(100-DISCOUNT_RATE)/100) FEE
+     FROM CAR_RENTAL_COMPANY_CAR 
+     INNER JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN 
+     USING (CAR_TYPE)
+     WHERE DURATION_TYPE = '30일 이상'
+     AND CAR_TYPE IN ('세단', 'SUV'))
+WHERE CAR_ID NOT IN (SELECT DISTINCT CAR_ID 
+              FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY 
+              WHERE END_DATE >= TO_DATE('2022-11-01', 'yyyy-mm-dd')
+              AND START_DATE <= TO_DATE('2022-11-30', 'yyyy-mm-dd'))
+AND FEE >= 500000 AND FEE < 2000000
+ORDER BY FEE DESC, CAR_TYPE, CAR_ID DESC
